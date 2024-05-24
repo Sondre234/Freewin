be901073f84987b14e999e890a0bec2e0e8c5ca6
sondre234 <sondrefmyrmel@gmail.com>
2024-05-24 05:57:39 +0200

    meanwhile fix

--- main.go
+++ main.go
@@ -1,42 +1,73 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Film struct {
-	Title    string
-	Director string
+type Comment struct {
+	ID      string    `bson:"_id"`
+	Name    string    `bson:"name"`
+	Email   string    `bson:"email"`
+	MovieID string    `bson:"movie_id"`
+	Text    string    `bson:"text"`
+	Date    time.Time `bson:"date"`
 }
 
 func main() {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		films := map[string][]Film{
+	clientOptions := options.Client().ApplyURI("mongodb+srv://sondre:passord@easywin.ihpovx4.mongodb.net/?retryWrites=true&w=majority&appName=Easywin").SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB: ", err)
+	}
 
-			"Films": {
-				{Title: "The Godfather", Director: "Francis Ford Coppola"},
-				{Title: "Blade Runner", Director: "Ridley Scott"},
-				{Title: "The thing", Director: "John Carpenter"},
-			},
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("Error pinging MongoDB: ", err)
+	}
+
+	log.Println("Connected to MongoDB!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		collection := client.Database("sample_mflix").Collection("comments")
+		var comments []Comment
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		cursor, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			log.Fatal("Error finding documents: ", err)
 		}
-		tmpl.Execute(w, films)
-	}
 
-	h2 := func(w http.ResponseWriter, r *http.Request) {
+		defer cursor.Close(ctx)
+		for cursor.Next(ctx) {
+			var comment Comment
+			if err := cursor.Decode(&comment); err != nil {
+				log.Fatal("Error decoding document: ", err)
+			}
+			comments = append(comments, comment)
+		}
 
-		title := r.PostFormValue("title")
-		director := r.PostFormValue("director")
+		if err := cursor.Err(); err != nil {
+			log.Fatal("Cursor error: ", err)
+		}
+
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+		if err := tmpl.Execute(w, comments); err != nil {
+			log.Fatal("Error executing template: ", err)
+		}
+	})
 
-	}
-
-	http.HandleFunc("/", h1)
-	http.HandleFunc("/add-film", h2)
-
-	log.Fatal(http.ListenAndServe("127.0.0.1:8000", nil))
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
