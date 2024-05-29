be0ccccb2438a85e1c8f11ac7fb8604ce7e5a83a
sondre234 <sondrefmyrmel@gmail.com>
2024-05-29 20:42:51 +0200

    get Info on users most played champions printed in console

--- src/Main.go
+++ src/Main.go
@@ -10,10 +10,11 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func main() {
+func main1() {
 
 	config, err := readConfig("resources/config.json")
 	if err != nil {
@@ -35,10 +36,18 @@ func main() {
 
 	log.Println("Starting server on :8000")
 	log.Fatal(http.ListenAndServe("127.0.0.1:8000", nil))
+
 }
 
 type Stats struct {
 	Stats []Champion `json:"stats"`
+}
+
+type Champs struct {
+	ChampionId     int  `json:"championId"`
+	ChampionLevel  int  `json:"championLevel"`
+	ChampionPoints int  `json:"championPoints"`
+	ChestGranted   bool `json:"chestGranted"`
 }
 
 type Champion struct {
@@ -55,6 +64,75 @@ type Summoner struct {
 	Puuid    string `json:"puuid"`
 	GameName string `json:"gameName"`
 	TagLine  string `json:"tagLine"`
+}
+
+func getChampionNameByID(id int) (string, error) {
+	file, err := os.Open("resources/champids.txt")
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, "\t")
+		if len(parts) == 2 {
+			champID, err := strconv.Atoi(parts[0])
+			if err != nil {
+				return "", err
+			}
+			if champID == id {
+				return parts[1], nil
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	return "Champion not found", nil
+}
+
+func main() {
+	// Get PUUID and champion info
+	name, err := getChampionNameByID(266)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Champion Name:", name)
+	}
+
+	res, err := http.Get("https://euw1.api.riotgames.com/lol/champion-mastery/v4/" +
+		"champion-masteries/by-puuid/vosf4Gq_pWSOBD-7jqnqupV1ZCNvbS66k10cDcIVJjjkiI6rjl03_-OK5acnbULt3ng3xRDGvZeYNA?api_key=RGAPI-277d267d-c27e-4411-a87a-df4502edc571")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var champs []Champs
+	json.Unmarshal(body, &champs)
+
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("unexpected status code: %d", res.StatusCode)
+	}
+
+	for i, p := range champs {
+		championName, err := getChampionNameByID(p.ChampionId)
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		fmt.Println("Champion", (i + 1), ":", championName)
+		fmt.Println("ChampionLevel: ", p.ChampionLevel)
+		fmt.Println("ChampionPoints: ", p.ChampionPoints)
+		fmt.Println("ChestGranted: ", p.ChestGranted)
+	}
 }
 
 func readConfig(filePath string) (*Config, error) {
